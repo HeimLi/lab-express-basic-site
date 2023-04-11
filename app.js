a843b07c5aca7e9fd562194a75998118f429e43a
HeimLi <127536715+HeimLi@users.noreply.github.com>
2023-04-11 17:10:24 +0200

    finished

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// Server Kreation: "Express" = Modul, d.h. imortiert others work in Javascript
+
 const express = require("express")
 const app = express()
-const port = 3000;
+// const port = 3000;
 
 
 // Register the public folder and send to client
@@ -8,18 +10,20 @@ app.use(express.static(__dirname + "/public"))
 
 // Create the different routes for the website
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/view/home.html")
+    res.sendFile(__dirname + "/views/index.html")
 })
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/view/works.html")
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/works.html")
 })
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/view/about.html")
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html")
 })
 
-app.listen(port, () => {
+//Portrunning = 3000
+
+app.listen(3000, () => {
     
 })
 
